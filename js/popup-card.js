3390e79906b8cd0bcb851e533ff769c952a2bdb2
Artem Aksentenko <akstemes@gmail.com>
2018-07-03 20:10:53 +0300

    Загрузка фотографий, избавление от неиспользуемых фрагментов кода

--- js/popup-card.js
+++ js/popup-card.js
@@ -9,7 +9,7 @@
     window.configPinsBlock.removeActivePin();
   };
   var cardEscPressHandler = function (evt) {
-    if (evt.keyCode === window.utils.ESC_KEY_CODE) {
+    if (evt.keyCode === window.ESC_KEY_CODE) {
       closeCard();
     }
     document.removeEventListener('keydown', cardEscPressHandler);
@@ -29,7 +29,7 @@
     cardElement.querySelector('.popup__title').textContent = estateObject.offer.title;
     cardElement.querySelector('.popup__text--address').textContent = estateObject.offer.addres;
     cardElement.querySelector('.popup__text--price').textContent = estateObject.offer.price + ' \u20bd' + '/ночь';
-    cardElement.querySelector('.popup__type').textContent = window.dataBlock.convertOfferType(estateObject.offer.type);
+    cardElement.querySelector('.popup__type').textContent = convertOfferType(estateObject.offer.type);
     cardElement.querySelector('.popup__text--capacity').textContent = estateObject.offer.rooms + ' комнаты для ' + estateObject.offer.guests + ' гостей';
     cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + estateObject.offer.checkin + ', выезд до ' + estateObject.offer.checkout;
     cardElement.querySelector('.popup__features').innerHTML = '';
@@ -66,6 +66,24 @@
     }
     return featureFragment;
   };
+  var convertOfferType = function (type) {
+    var offerType = '';
+    switch (type) {
+      case 'flat':
+        offerType = 'Квартира';
+        break;
+      case 'bungalo':
+        offerType = 'Бунгало';
+        break;
+      case 'house':
+        offerType = 'Дом';
+        break;
+      case 'palace':
+        offerType = 'Дворец';
+    }
+    return offerType;
+  };
+
   var mapPinTemplate = document.querySelector('template');
   var mapCard = mapPinTemplate.content.querySelector('.map__card');
   var photosGroup = mapCard.querySelector('.popup__photos');
